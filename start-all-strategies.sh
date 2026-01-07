5d85a235a48e33629ee9d4a7eebbb454c5c724f5
MYPC\46745 <467456744@qq.com>
2026-01-07 14:58:38 +0800

    x

--- start-all-strategies.sh
+++ start-all-strategies.sh
@@ -13,7 +13,7 @@ ACTION=${1:-up}
 
 STRATEGIES=(
     SMA RSI MACD ATRBreakout BollingerWidth VolatilityRegime
-    OrderFlow MultiTimeframe CrossExchangeSpread StatisticalArbitrage
+    OrderFlow MultiTimeframe
 )
 
 # 端口基数 (每个策略递增)
